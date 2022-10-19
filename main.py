cb6bb524ec3ceefa8a7ce9468762b8cee7889ca8
Panchito <daniel.carrillo90@uabc.edu.mx>
2022-10-18 19:22:05 -0700

    COMMIT3 - Fix welcome message in main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 # OUTPUT DEL SCRIPT:
 
 
-print("Hola new pythonista hay un tremendo error en esta línea\n
-"Agrega unas benditas \" al final de la primer linea.\n"
+print("Hola new pythonista hay un tremendo error en esta línea\n"
+"Agrega unas benditas \ al final de la primer linea.\n"
 "El profe me hace los mandados. SOY UN CRACK EN GIT (っ▀¯▀)つ.")
